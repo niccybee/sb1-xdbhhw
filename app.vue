4345e84b004f54e446128fc3417f52be5344861d
niccybee <niccybee@users.noreply.github.com>
2024-12-16 06:49:51 +0000

    Restored to 'd053ba79d652baa376eb6b3574f7cd1499c55380'
    Replit-Restored-To:d053ba79d652baa376eb6b3574f7cd1499c55380

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import { useKeyboard } from './composables/initKeyboard'
-import { useToast } from 'vue-toastification' // Assuming you are using a toast library like vue-toastification
-
-const { toasts } = useToast(); // Initialize the toast composable
 
 onMounted(() => {
   useKeyboard()
